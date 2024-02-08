4ec731d5617c2ddf42adc153abb7e8f6f4b4e33f
NigarRahimli <nigarrahimli07@gmail.com>
2024-02-08 23:39:50 +0400

    Apointment list design is improved

--- src/pages/AppointmentsList.js
+++ src/pages/AppointmentsList.js
@@ -1,3 +1,13 @@
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import React, { useState, useEffect } from "react";
 
 function AppointmentsList() {
@@ -23,20 +33,43 @@ function AppointmentsList() {
   };
 
   return (
-  
     <div>
-      
       <h1>Appointments</h1>
-      <ul>
-        {appointments.map((appointment) => (
-          <li key={appointment.id}>
-            <div>Doctor ID: {appointment.doctorId}</div>
-            <div>Start Time: {appointment.startTime}</div>
-            <div>End Time: {appointment.endTime}</div>
-            <div>Patient Name: {appointment.patientName}</div>
-          </li>
-        ))}
-      </ul>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 600 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Id </TableCell>
+              <TableCell align="center">Patient</TableCell>
+              <TableCell align="center">Doctor</TableCell>
+              <TableCell align="center">Starts at</TableCell>
+              <TableCell align="center">Ends at</TableCell>
+              <TableCell align="center"></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {appointments.map((appointment) => (
+              <TableRow
+                key={appointment.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {appointment.id}
+                </TableCell>
+                <TableCell align="center">{appointment.patientName}</TableCell>
+                <TableCell align="center">{appointment.doctorId}</TableCell>
+                <TableCell align="center">{appointment.startTime}</TableCell>
+                <TableCell align="center">{appointment.endTime}</TableCell>
+                <TableCell align="center">
+                  <Button variant="contained" color="error">
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
